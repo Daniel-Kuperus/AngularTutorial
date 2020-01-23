8bba197e8361e7383cdfacebabda5d8d6d1f0c54
DanielKuperus <daniel.kuperus@gmail.com>
2020-01-23 13:27:42 +0100

    Angular Tutorial Opdracht 3 is klaar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
-import { FormsModule } from '@angular/forms'; // <-- FormsModule zorgt ervoor dat fields editable zijn.
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- FormsModule zorgt ervoor dat fields editable zijn.
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
